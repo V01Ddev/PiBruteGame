1c230ef8951e347cb779361622d9e71ab1a8a67f
V01Ddev <x.v.0.1.d.xhacking@gmail.com>
2023-09-22 12:59:06 +0300

    Fixed missing bracket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from itertools import permutations
+from itertools import product
 import random
 import time
 
@@ -11,14 +11,19 @@ GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def perms():
-    perms_list = list(permutations(range(1, 4)))
-    rand = random.randint(0, 5)
-    print(perms_list[rand])
-    return list(perms_list[rand])
+    options = [1, 0] # the states the switch can have, high or low
+    n = 3 # the number of switches in the game
+    perms_l = list(product(*(options,)*n))
+
+    print(perms_l)
+    print(len(perms_l))
+    rand = random.randint(0, len(perms_l))
+    print(perms_l[rand])
+    return list(perms_l[rand])
 
 
 
-def buttons:
+def buttons():
     try:
         while True:
             time.sleep(0.1)
@@ -36,7 +41,6 @@ def main():
     print(perms)
 
     
-
 
 if __name__ == "__main__":
     perms()
