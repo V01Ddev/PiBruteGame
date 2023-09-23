e5ebc4a9a43c7a7313bc8e320b8f5ec44692ce8b
V01Ddev <x.v.0.1.d.xhacking@gmail.com>
2023-09-23 22:17:47 +0300

    Added switch checking and general code structure

--- main.py
+++ main.py
@@ -1,46 +1,94 @@
 import RPi.GPIO as GPIO
+from guizero import App, Text, PushButton
 from itertools import product
 import random
 import time
 
 
-BUTTON_PIN = 16
+"""
+Time:
+    * take time when function first starts running using time module find time after code is broken then get the delta and display it.
+GUI:
+    * Use Guizero to make a pop up for when the code is broken
+    * Start again button to reset
+
+"""
+
+
+
+SWITCH_1 = 17
+SWITCH_2 = 27
+SWITCH_3 = 22
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.setup(SWITCH_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SWITCH_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SWITCH_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 
-def perms():
+def code_gen():
     options = [1, 0] # the states the switch can have, high or low
     n = 3 # the number of switches in the game
+
     perms_l = list(product(*(options,)*n))
 
-    print(perms_l)
-    print(len(perms_l))
     rand = random.randint(0, len(perms_l))
-    print(perms_l[rand])
     return list(perms_l[rand])
 
 
 
-def buttons():
-    try:
-        while True:
-            time.sleep(0.1)
-            if GPIO.input(BUTTON_PIN) == GPIO.LOW:
-                print("Button is pressed")
-            else:
-                print("Nothing can be see...")
+def ButtonStats():
+
+    sw = []
+
+    if GPIO.input(SWITCH_1) == GPIO.LOW:
+        sw.append(1)
+    else:
+        sw.append(0)
+    
+    if GPIO.input(SWITCH_2) == GPIO.LOW:
+        sw.append(1)
+    else:
+        sw.append(0)
+
+    if GPIO.input(SWITCH_3) == GPIO.LOW:
+        sw.append(1)
+    else:
+        sw.append(0)
+    
+    return sw
+
+
+
+def game():
+
+    code = code_gen()
+    
+    for i in range(3):
+        print("Game stating in {i}...")
+        time.sleep(1)
+
+    start_time = time.time()
+    While game_state = True:
+        if ButtonStats() == code:
+            print(f"CODE {code} BROKEN!")
+            print(time.time() - start_time)
+
+
+
+def main():
+    game()
+
+    while True():
+        res = str(input("Press enter to restart (ctrl-C to quit) "))
+        game()
+
     except KeyboardInterrupt:
         GPIO.cleanup
 
 
 
-def main():
-    Lperms = perms()
-    print(perms)
-
-    
-
 if __name__ == "__main__":
-    perms()
+    main() 
